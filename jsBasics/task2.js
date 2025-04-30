5885c826b600eb42a8cd9d1065b63394f3f6ff03
Jokaen <kipsempoint@gmail.com>
2025-04-30 13:23:44 +0300

    Добавили нову папку з файлами в яких виконані задачі

---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+let nameFirstGuest = "Antonio De Luviazye";
+let nameSecondGuest = "Boris";
+
+let welcomeMessage = `Welcome aboard dear ${nameFirstGuest} and ${nameSecondGuest} !`;
+
+console.log(welcomeMessage);
---
+++ jsBasics/task4_2.js
@@ -0,0 +1,5 @@
+const length = 5;
+const width = 10;
+
+const square = length * width;
+console.log(`Площадь прямокутника: ${square}`);
